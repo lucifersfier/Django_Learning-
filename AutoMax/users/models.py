4fb0066fb8d6403d6319bb51d1607cdd290c746d
Lucifersfier <nssrivas@gmail.com>
2023-11-04 01:15:51 +0530

    Creating profile model is done

--- AutoMax/users/models.py
+++ AutoMax/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  #we are going to create a property on our profile,
+    photo = models.ImageField(null=True)                         #which i'm going to call user and this is going to
+    bio = models.CharField(max_length=140, blank=True)           # be equal to models.OneToOneField   
+    phone_number= models.CharField(max_length=10,blank=True)        
+     
+    
