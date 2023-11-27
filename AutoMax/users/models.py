fce8ddf7f5a128df3a7786b517feb39d6692644f
Lucifersfier <nssrivas@gmail.com>
2023-11-27 23:50:43 +0530

    signals in Django is done for this project

--- AutoMax/users/models.py
+++ AutoMax/users/models.py
@@ -9,4 +9,4 @@ class Profile(models.Model):
     phone_number= models.CharField(max_length=10,blank=True)        
      
     def __str__(self):
-        return f'{self.user.username}\'s Profile'
+        return f'{self.user.username}\'s Profile' 
--- AutoMax/users/signals.py
+++ AutoMax/users/signals.py
@@ -1,3 +1,13 @@
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+#now i am going to be creating a function which is basically going to run when our user gets creates or when a signal basically gets made.
 from .models import Profile
+@receiver(post_save, sender=User)
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+     
