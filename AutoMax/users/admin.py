8b09e272013641e2d89892569eddd5c11a50c37a
Lucifersfier <nssrivas@gmail.com>
2024-01-10 01:47:54 +0530

    Adding Location Data to PROFILE is done

--- AutoMax/users/admin.py
+++ AutoMax/users/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile
+from .models import Profile, Location
 
 '''i am going to basically create a class which i am going to call profile admin '''
 
 class ProfileAdmin(admin.ModelAdmin):
     pass
+class LocationAdmin(admin.ModelAdmin):
+    pass
 #register function register the model for the admin panel so that the admin panel doesn't displays it.
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Location, LocationAdmin)
--- AutoMax/users/models.py
+++ AutoMax/users/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from localflavor.in_.models import INStateField, INPANCardNumberField
+
+class Location(models.Model):
+    address1 = models.CharField(max_length=128, blank=True)
+    address2 = models.CharField(max_length=128, blank=True)
+    city = models.CharField(max_length=64)  
+    state = INStateField(default="Delhi")
+    pan_card = INPANCardNumberField(blank=True)
+
+def __str__(self):
+    return f'Location {self.id}'
+    
 
 # Create your models here.
 class Profile(models.Model):
@@ -7,6 +19,9 @@ class Profile(models.Model):
     photo = models.ImageField(null=True)                         #which i'm going to call user and this is going to
     bio = models.CharField(max_length=140, blank=True)           # be equal to models.OneToOneField   
     phone_number= models.CharField(max_length=10,blank=True)        
+    location = models.OneToOneField(Location, on_delete=models.CASCADE, null=True)
+    
+    
      
     def __str__(self):
         return f'{self.user.username}\'s Profile' 
